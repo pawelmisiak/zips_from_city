73d2942951ec5aa027ddcc6a12c8a8fc40529df0
pawelmisiak <thepawelmisiak@gmail.com>
2019-06-10 11:45:28 -0400

    copied code from zip-to-city to modify it. Now will try to change the api to get zip from city instead

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theText: "Nothing to display"
+    };
+  }
+
+  cityLook() {
+    const text = document.getElementById("inputHolder").value;
+    let city = "";
+    if (text.length < 5) {
+      return this.state.theText;
+    } else if (text.length === 5) {
+      fetch(`http://ctp-zip-api.herokuapp.com/zip/${text}`)
+        .then(result => {
+          return result.json();
+        })
+        .then(r => {
+          console.log(r[0].City);
+          city = r[0].City;
+          this.setState({ theText: city });
+        })
+        .catch(() => {
+          console.log("error");
+          this.setState({ theText: "No Such City" });
+        });
+    }
+    return;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi there</h1>
+        <header className="App-header">
+          <h3>Lets find the city you are looking for</h3>
+          <form>
+            <label>Zip Code: </label>
+            <input
+              type="text"
+              id="inputHolder"
+              onChange={e => {
+                this.cityLook();
+              }}
+            />
+            <p>{this.state.theText}</p>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
