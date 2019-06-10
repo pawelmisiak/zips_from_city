e04169303c20aa8a9383733576515b1da810ff77
pawelmisiak <thepawelmisiak@gmail.com>
2019-06-10 12:20:11 -0400

    changed API retrival, added conditional and now it runs only on enter key press

--- src/App.js
+++ src/App.js
@@ -5,50 +5,52 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      theText: "Nothing to display"
+      theText: "Nothing to display",
+      zips: []
     };
   }
 
-  cityLook() {
-    const text = document.getElementById("inputHolder").value;
-    let city = "";
-    if (text.length < 5) {
-      return this.state.theText;
-    } else if (text.length === 5) {
-      fetch(`http://ctp-zip-api.herokuapp.com/zip/${text}`)
+  cityLook(event) {
+    if (event.key === "Enter") {
+      const text = document.getElementById("inputHolder").value.toUpperCase();
+      let city = "";
+
+      fetch(`http://ctp-zip-api.herokuapp.com/city/${text}`)
         .then(result => {
           return result.json();
         })
         .then(r => {
-          console.log(r[0].City);
-          city = r[0].City;
-          this.setState({ theText: city });
+          console.log(r[2]);
+          city = r;
+          this.setState({ zips: city });
         })
         .catch(() => {
           console.log("error");
-          this.setState({ theText: "No Such City" });
+          this.setState({ zips: "No Such City" });
         });
     }
     return;
+  }
+  getZips() {
+    return this.state.zips.map(zip => <li>{zip}</li>);
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Hi there</h1>
         <header className="App-header">
           <h3>Lets find the city you are looking for</h3>
-          <form>
-            <label>Zip Code: </label>
-            <input
-              type="text"
-              id="inputHolder"
-              onChange={e => {
-                this.cityLook();
-              }}
-            />
-            <p>{this.state.theText}</p>
-          </form>
+
+          <label>Enter City:</label>
+          <input
+            type="text"
+            id="inputHolder"
+            onKeyPress={e => {
+              this.cityLook(e);
+            }}
+          />
+          {/* <p className="output">{this.state.zips}</p> */}
+          <ol className="output">{this.getZips()}</ol>
         </header>
       </div>
     );
